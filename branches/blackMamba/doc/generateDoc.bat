19a75418f9449b38d97639827929753bcfc55e74
serge.beucher <serge.beucher@gmail.com>
2014-07-13 17:24:09 +0000

    Batch file for generating doc on Windows modified. Better but not perfect yet...

--- branches/blackMamba/doc/generateDoc.bat
+++ branches/blackMamba/doc/generateDoc.bat
@@ -10,45 +10,48 @@ Rem all the files in directory style/ (mamba latex style mamba.sty, logo image
 Rem mamba_logo.png and license icon by.pdf) are foundable as a package for miktex
 
 Rem USER MANUAL
-copy style\mamba_logo.pdf userman\
-copy style\by.pdf userman\
-pushd userman\
-python examples2tex.py
+copy style\mamba_logo.pdf mamba-um\
+copy style\by.pdf mamba-um\
+rem copy style\mamba.sty mamba-um\
+pushd mamba-um
+rem python examples2tex.py
+pdflatex mamba-um.tex
 pdflatex mamba-um.tex
 pdflatex mamba-um.tex
 copy mamba-um.pdf ..
 del /Q/F mamba_logo.pdf
 del /Q/F by.pdf
+rem del /Q/F mamba.sty
 popd 
 
 Rem PYTHON REFERENCE
-copy style\mamba_logo.pdf py_ref\
-copy style\by.pdf py_ref\
-pushd py_ref
+copy style\mamba_logo.pdf mamba-pyref\
+copy style\by.pdf mamba-pyref\
+pushd mamba-pyref
 python createPythonRef.py
-copy pyref.pdf ..
+copy mamba-pyref.pdf ..
 python createPythonQuickRef.py
-copy pyquickref.pdf ..
+copy mamba-pyquickref.pdf ..
 del /Q/F mamba_logo.pdf
 del /Q/F by.pdf
 popd
 
 Rem STANDARD
-copy style\mamba_logo.pdf standards\
-copy style\by.pdf standards\
-pushd standards
-pdflatex standards.tex
-pdflatex standards.tex
-copy standards.pdf ..
+copy style\mamba_logo.pdf mamba-std\
+copy style\by.pdf mamba-std\
+pushd mamba-std
+pdflatex mamba-std.tex
+pdflatex mamba-std.tex
+copy mamba-std.pdf ..
 del /Q/F mamba_logo.pdf
 del /Q/F by.pdf
 popd
 
 Rem API REFERENCE
-pushd mambaapi_ref
+pushd mamba-cref
 doxygen mamba.cfg
-copy ..\style\mamba_logo.png html
-python createDoxygenSty.py
+rem copy ..\style\mamba_logo.png html
+rem python createDoxygenSty.py
 cd latex
 copy ..\..\style\mamba_logo.pdf .
 copy ..\..\style\by.pdf .
